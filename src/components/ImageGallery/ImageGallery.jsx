61c1027fb913be5d6c8d5544753d56083bd56e9d
AnastasiiaAnosova <118612211+AnastasiiaAnosova@users.noreply.github.com>
2023-12-27 16:26:19 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem'
 import { ImageGalleryContainer } from './ImageGallery.styled'
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 
 
 const ImageGallery = ({ images, toogleModal }) => {
     return (
         <ImageGalleryContainer>
-            {images.map(({ webformatURL, largeImageURL, tags }) => (
+            {images.map(({ id, webformatURL, largeImageURL, tags }) => (
                 <ImageGalleryItem
-                    key={nanoid()}
+                    key={id}
                     webformatURL={webformatURL}
                     alt={tags}
                     largeImageURL={largeImageURL}
